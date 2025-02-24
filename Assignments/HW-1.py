6e243e42b12604a04dd5109159744be1fe6f1eb7
Matthew Kuilan <56884689+MatthewKuilan@users.noreply.github.com>
2025-02-24 10:38:05 -0500

    Update HW-1.py

--- Assignments/HW-1.py
+++ Assignments/HW-1.py
@@ -5,18 +5,6 @@ def double(y: int) -> int:
     return y * 2
 
 
-class TestDoubleMethod(unittest.TestCase):
-    def test_Double(self):
-        self.assertEqual(double(3), 6)
-        self.assertEqual(double(0), 0)
-        self.assertEqual(double(-2), 4)
-
-    # context manager, ensures error is raised if wrong type is used
-    def test_invalidtype(self):
-        with self.assertRaises(TypeError):
-            double("Stuff")
-
-
 def greet(x: str) -> str:
     for _ in range(10):
         print("hi")
@@ -28,5 +16,17 @@ test = greet("Matthew")
 print(test)
 
 
+class TestDoubleMethod(unittest.TestCase):
+    def test_Double(self):
+        self.assertEqual(double(3), 6)
+        self.assertEqual(double(0), 0)
+        self.assertEqual(double(-2), 4)
+
+    # test case for invalid type
+    def test_invalidtype(self):
+        with self.assertRaises(TypeError):
+            double("Stuff")
+
+# ensures tests run
 if __name__ == "__main__":
     unittest.main()
