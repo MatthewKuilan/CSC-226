c07e8abe54f453de69432f431e5a7bf99b8fa4b3
MatthewKuilan <56884689+MatthewKuilan@users.noreply.github.com>
2025-04-25 22:15:58 -0400

    More flask setup
    added template and static folders, template holds html, static holds css and js

--- Assignments/flask-test/app.py
+++ Assignments/flask-test/app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template 
 
 app = Flask(__name__)
 
-@app.route('/submit-get')
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit-get/')
 def submit_get():
     name = request.args.get('username')
     return f"Hello from GET, {name}!"
