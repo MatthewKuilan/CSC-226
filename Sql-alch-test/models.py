5e5047abc0a47a1527881704a284ef212ff77139
Matthew Kuilan <56884689+MatthewKuilan@users.noreply.github.com>
2025-05-10 14:47:26 -0400

    .

--- Sql-alch-test/models.py
+++ Sql-alch-test/models.py
@@ -6,4 +6,4 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
-    answer_choice: Mapped[str] =  mapped_column()
+    choice: Mapped[str] =  mapped_column()
