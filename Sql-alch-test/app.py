21d3c50114c92bef2b743df134525202033d0b9b
Matthew Kuilan <56884689+MatthewKuilan@users.noreply.github.com>
2025-05-17 17:12:03 -0400

    .

--- Sql-alch-test/app.py
+++ Sql-alch-test/app.py
@@ -3,7 +3,7 @@ from extensions import db
 from models import User
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:/Users/matthew.kuilan/Documents/GitHub/CSC-226/instance/mydb.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:/Users/matthew.kuilan/Documents/GitHub/CSC-226"
 db.init_app(app)
 
 @app.route("/")
